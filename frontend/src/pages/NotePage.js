eba1174586b82abd0cc2d85d589371b03d0ab827
Danilo Tanaka <danilotanaka@Danilos-MacBook-Air.local>
2023-06-25 21:31:20 -0300

    Create Delete Button.

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { csrftoken } from '../components/csrfToken'
 
-const NotePage = ({ match }) => {
+const NotePage = ({ match, history }) => {
 
   let { id } = useParams();
   let [note, setNote] = useState(null)
@@ -18,6 +18,8 @@ const NotePage = ({ match }) => {
     setNote(data)
   }
 
+  let navigate = useNavigate()
+
   let updateNote = async () => {
     let response = await fetch(`/api/notes/${id}/`, {
       method: "PUT",
@@ -27,7 +29,18 @@ const NotePage = ({ match }) => {
       },
       body: JSON.stringify(note)
     })
-    
+    navigate('/')
+  }
+
+  let deleteNote = async () => {
+    let response = await fetch(`/api/notes/${id}`, {
+      method: "DELETE",
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken
+      },
+    })
+    navigate('/')
   }
     
   return (
@@ -35,8 +48,9 @@ const NotePage = ({ match }) => {
       <textarea onChange={(e) => { setNote({...note, 'title': e.target.value})}} defaultValue={note?.title}></textarea>
       <textarea onChange={(e) => { setNote({...note, 'title': e.target.value})}} defaultValue={note?.content}></textarea>
       <button onClick={updateNote}>Save</button>
+      <button onClick={deleteNote}>Delete</button>
     </div>
   )
-}
+} 
 
 export default NotePage
