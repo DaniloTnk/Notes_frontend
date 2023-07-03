a1a03eb8308209c905c96093ca858345be837c21
Danilo Tanaka <danilotanaka@Danilos-MacBook-Air.local>
2023-07-03 12:39:02 -0300

    Add docker on project.

--- frontend/src/pages/NoteListPage.js
+++ frontend/src/pages/NoteListPage.js
@@ -11,7 +11,7 @@ const NoteListPage = () => {
 
     let getNotes = async () => {
 
-        let response = await fetch('api/notes/');
+        let response = await fetch('/api/notes/');
         let data = await response.json();
         setNotes(data)
     }
